95e5be5b26ee0d0897165d044e7a82c9b17dbd56
Anurag <anuragsain890@gmail.com>
2021-07-29 15:13:23 +0530

    _end.0_

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import {uuid} from "uuidv4";
 import Header from "./components/Header";
 import AddContact from "./components/AddContact";
 import ContactList from "./components/ContactList";
@@ -8,13 +9,31 @@ function App() {
   const [contacts, setContacts] = useState([]);
   const addContactHandler = (contact) => {
     console.log(contact);
-    setContacts([...contacts, contact]);
+    setContacts([...contacts, {id: uuid(), ...contacts} ,contact]);
   };
+
+  const removeContactHandler = (id) => {
+    const newContact = contacts.filter((contact) => {
+      return contact.id !== id;  
+    });
+    setContacts(newContact);
+  };
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(
+      localStorage.getItem("LOCAL_STORAGE_KEY")
+    );
+    if (retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("LOCAL_STORAGE_KEY", JSON.stringify(contacts));
+  }, [contacts]);
   return (
     <div className="ui container">
       <Header />
       <AddContact addContactHandler={addContactHandler} />
-      <ContactList contacts={contacts} />
+      <ContactList contacts={contacts} getContactId= {removeContactHandler}/>
     </div>
   );
 }
--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,15 +1,35 @@
 import React from "react";
 
 class AddContact extends React.Component {
+  state = {
+    name: "",
+    email: "",
+  };
+  add = (i) => {
+    i.preventDefault();
+    if (this.state.name === "" || this.state.email === "") {
+      alert("fields are empty!");
+      return;
+    } else {
+      this.props.addContactHandler(this.state);
+      this.setState({ name: "", email: "" });
+    }
+  };
   render() {
     return (
       <div className="ui main">
         <h2>Add Contact</h2>
 
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.add}>
           <div className="field">
             <label>Name</label>
-            <input type="text" name="name" placeholder="eg:Name"></input>
+            <input
+              type="text"
+              name="name"
+              placeholder="eg:Name"
+              value={this.state.name}
+              onChange={(i) => this.setState({ name: i.target.value })}
+            />
           </div>
           <div className="field">
             <label>Email</label>
@@ -17,7 +37,9 @@ class AddContact extends React.Component {
               type="text"
               name="email"
               placeholder="eg:Name@email.com"
-            ></input>
+              value={this.state.email}
+              onChange={(i) => this.setState({ email: i.target.value })}
+            />
           </div>
           <button className="ui button blue">Add</button>
         </form>
